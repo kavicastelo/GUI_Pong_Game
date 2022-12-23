9ca3d600d06247fcf4d7cce01214ddef0090f5cf
KAVI <kavindu123450@gmail.com>
2022-12-24 02:31:09 +0530

    5

--- firstGame/game.cpp
+++ firstGame/game.cpp
@@ -14,7 +14,7 @@ static void simulate_player(float *p, float *dp, float ddp, float time) {
 	ddp -= *dp * 10.f;
 
 	*p = *p + *dp * time + ddp * time * time * .5f;
-	*dp = *dp + ddp * time;
+	*dp = *dp + ddp * time; 
 
 	if (*p + player_half_size_y > arena_half_size_y) {
 		*p = arena_half_size_y - player_half_size_y;
@@ -29,7 +29,7 @@ static void simulate_player(float *p, float *dp, float ddp, float time) {
 static void simulate_game(Input* input, float time) {
 	clear_screen(0xff5500);
 
-	draw_rectangle(0, 0, arena_half_size_x, arena_half_size_y, 0xf1c40f);
+	draw_rectangle(0, 0, arena_half_size_x, arena_half_size_y, 0xf1c40f); // main background
 
 	float player_1_ddp = 0.f;
 	if (is_down(BUTTON_UP)) player_1_ddp += 1000;
@@ -42,51 +42,54 @@ static void simulate_game(Input* input, float time) {
 	simulate_player(&player_1_p, &player_1_dp, player_1_ddp, time);
 	simulate_player(&player_2_p, &player_2_dp, player_2_ddp, time);
 
-	ball_p_x += ball_dp_x * time;
-	ball_p_y += ball_dp_y * time;
+	// ball simulate
+	
+		ball_p_x += ball_dp_x * time;
+		ball_p_y += ball_dp_y * time;
 
-	if (ball_p_x + ball_half_size > 40 - player_half_size_x && 
-		ball_p_x - ball_half_size < 40 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_1_p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_1_p + player_half_size_y ) {
+		if (ball_p_x + ball_half_size > 40 - player_half_size_x &&
+			ball_p_x - ball_half_size < 40 + player_half_size_x &&
+			ball_p_y + ball_half_size > player_1_p - player_half_size_y &&
+			ball_p_y + ball_half_size < player_1_p + player_half_size_y) {
 
-		ball_p_x = 40 - player_half_size_x - ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
-	}
-	else if (ball_p_x + ball_half_size > -40 - player_half_size_x &&
-		ball_p_x - ball_half_size < -40 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_2_p + player_half_size_y) {
+			ball_p_x = 40 - player_half_size_x - ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
+		}
+		else if (ball_p_x + ball_half_size > -40 - player_half_size_x &&
+			ball_p_x - ball_half_size < -40 + player_half_size_x &&
+			ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
+			ball_p_y + ball_half_size < player_2_p + player_half_size_y) {
 
-		ball_p_x = -40 + player_half_size_x + ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * .75f;
-	}
+			ball_p_x = -40 + player_half_size_x + ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_2_p) * 2 + player_1_dp * .75f;
+		}
 
-	if (ball_p_y + ball_half_size > arena_half_size_y) {
-		ball_p_y = arena_half_size_y - ball_half_size;
-		ball_dp_y *= -1;
-	} 
-	else if (ball_p_y + ball_half_size < -arena_half_size_y) {
-		ball_p_y = -arena_half_size_y + ball_half_size;
-		ball_dp_y *= -1;
-	}
+		if (ball_p_y + ball_half_size > arena_half_size_y) {
+			ball_p_y = arena_half_size_y - ball_half_size;
+			ball_dp_y *= -1;
+		}
+		else if (ball_p_y + ball_half_size < -arena_half_size_y) {
+			ball_p_y = -arena_half_size_y + ball_half_size;
+			ball_dp_y *= -1;
+		}
 
-	if (ball_p_x + ball_half_size > arena_half_size_x) {
-		ball_dp_x *= -1;
-		ball_dp_y = 0;
-		ball_p_x = 0;
-		ball_p_y = 0;
-	}
-	else if (ball_p_x - ball_half_size < -arena_half_size_x) {
-		ball_dp_x *= -1;
-		ball_dp_y = 0;
-		ball_p_x = 0;
-		ball_p_y = 0;
-	}
+		if (ball_p_x + ball_half_size > arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
+		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
+	
 
-	draw_rectangle(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
-	draw_rectangle(40, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
-	draw_rectangle(-40, player_2_p, 1.5, 5, 0xff0000);
+	draw_rectangle(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // ball
+	draw_rectangle(40, player_1_p, player_half_size_x, player_half_size_y, 0xff0000); // playerPads
+	draw_rectangle(-40, player_2_p, 1.5, 5, 0xff0000); // container
 }
